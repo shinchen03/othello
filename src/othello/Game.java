cdc0042bcbd25f187941fd501434f8bbcdb1b7f6
shin <shinchen03@gmail.com>
2018-01-20 15:19:53 -0600

    first commit
    create the initial board

--- src/othello/Game.java
+++ src/othello/Game.java
@@ -2,15 +2,16 @@ package othello;
 
 public class Game {
 	public void initial() {
-		String[][] board = new String[9][9];
+		String[][] board = new String[10][10];
 		// create the board
-		for (int i=0; i<9; i++) {
-			for (int j=0; j<9; j++) {
+		for (int i=0; i<10; i++) {
+			for (int j=0; j<10; j++) {
 				if (i == 0) board[i][j] = String.valueOf(j);
+				if (j == 9) board[i][j] = "\n";
 				else if (j == 0) board[i][j] = String.valueOf(i);
 				else if (i == 5 && j == 4 || i == 4 && j == 5) board[i][j] ="›";
-				else if (i == 4 && j == 5 || i == 5 && j == 5) board[i][j] = "œ";
-				else board[i][j] = " ";
+				else if (i == 4 && j == 4 || i == 5 && j == 5) board[i][j] = "œ";
+				else if (i != 0) board[i][j] = " ";
 			}
 		}
 		printState(board);
@@ -18,9 +19,9 @@ public class Game {
 	
 	public void printState(String[][] board) {
 		// visualize game state in matrix
-		for (int i=0; i<9; i++) {
-			for (int j=9; j<9; j++) {
-				System.out.println(board[i][j]);
+		for (int i=0; i<10; i++) {
+			for (int j=0; j<10; j++) {
+				System.out.print(board[i][j]);
 			}
 		}
 	}
